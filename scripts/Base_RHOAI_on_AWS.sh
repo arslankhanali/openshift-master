481649b2f3415fbec1f6f2d2cfff52c3341c3e0b
Arslan <arslankhanali@gmail.com>
2024-03-10 15:01:59 +1100

    added base rhoai

--- scripts/Base_RHOAI_on_AWS.sh
+++ scripts/Base_RHOAI_on_AWS.sh
@@ -12,9 +12,9 @@ oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/mai
 sleep 2
 oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_nvidiagpu.yaml 
 
-#pipelines
-sleep 2
-oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_pipelines.yaml
+# #pipelines
+# sleep 2
+# oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_pipelines.yaml
 
 #servicemesh - Needed for Caikit TGIS serving runtime
 sleep 2
@@ -25,13 +25,12 @@ sleep 2
 oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_serverless.yaml 
 
 #codeflare - For Ray
-sleep 2
+sleep 10
 oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_codeflare.yaml  
 
 #minio - For s3 storage
 sleep 2
 oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/applications/minio_template.yaml
-
 
 #crds
 sleep 30
@@ -40,8 +39,8 @@ oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/mai
 oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/crds/odf_storagesystem.yaml
 
 #ai operator
-sleep 30
-oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_ai.yaml  
+# sleep 30
+# oc apply -f https://raw.githubusercontent.com/arslankhanali/openshift-helper/main/operators/operator_ai.yaml  
 
 # Values
 echo "https://$(oc get routes.route.openshift.io -n minio  minio-ui -o=jsonpath='{.spec.host}' )"
